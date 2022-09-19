aa1590d4a45e2eed37d22b9082820bb8e6a80bd2
bardi <bardia.mojra@gmail.com>
2022-09-19 10:25:02 -0500

    Updated with git-express on 2022-09-18.

--- dev/main.m
+++ dev/main.m
@@ -2,93 +2,35 @@
 %% init sys 
 %warning('off','all')
 close all; clear; clc;
-cfg  = cfg_class(TID    = ['T000','000','_piDMD_',''], ...
+cfg  = cfg_class(TID    = ['T000','000','_koop_',''], ...
                  brief  = ["."], ...
+                 btype  = "vtol", ...
                  bnum   = 1, ...
                  end_frame  = 200);
 dlgr  = dlgr_class(); dlgr.load_cfg(cfg);
-%rpt   = report_class(); rpt.load_cfg(cfg);
 %% init app modules
-pi    = piDMD_class(); pi.load_cfg(cfg); 
-% import SINDy
-%hvk   = HAVOK_class(); hvk.load_cfg(cfg);
-%knc   = KRONIC_class(); knc.load_cfg(cfg);
+kp    = koopman_class(); kp.load_cfg(cfg); % also loads dat 
 
 %% run
-gt_m      = model_class(mthd = "ground truth", rec = pi.dat); % gt
-pi_m  = pi.get_model(pi.X, pi.Y, "exact", ''); % piDMD baseline
-%A_m  = pi.get_model(pi.X, pi.Y, "piDMD orth", "orthogonal"); 
-%B_m  = pi.get_model(pi.X, pi.Y, "piDMD orth", "orthogonal"); 
+gt_m    = model_class(mthd = "ground truth", rec = pi.dat); % gt
 
-% piDMD methods B000
-%extS_mA = pi.get_model(pi.X, pi.Y, "exactSVDS",  "A"); % 01
-%orth_mA = pi.get_model(pi.X, pi.Y, "orthogonal", "A"); % 02 r
-%orth_mB = pi.get_model(pi.X, pi.Y, "orthogonal", "B:r40",40); % 02 r
-%D_mA  = pi.get_model(pi.X, pi.Y, "uppertriangular"); % 03 <<-------------- 
-%C_mA  = pi.get_model(pi.X, pi.Y, "lowertriangular"); % 04 <<-------------- 
-%diag_mA  = pi.get_model(pi.X,pi.Y, "diagonal", "A"); % 05
-%diag_mB  = pi.get_model(pi.X,pi.Y, "diagonal", "B:d2",2); % 05
-%diag_mC  = pi.get_model(pi.X,pi.Y, "diagonal", "C:d3",3); % 05
-%diag_mD  = pi.get_model(pi.X,pi.Y, "diagonal", "C:d4",4); % 05
-%diag_mE  = pi.get_model(pi.X,pi.Y, "diagonal", "C:d30",30); % 05
-%F_mA  = pi.get_model(pi.X,pi.Y, "diagonaltls"); % 07 <<-------------- 
-%C_mA  = pi.get_model(pi.X,pi.Y, "symtridiagonal"); % 08 <<-------------- 
-circ_mA = pi.get_model(pi.X,pi.Y, "circulant", "A"); % 09
-cTLS_mA = pi.get_model(pi.X,pi.Y, "circulantTLS", "A"); % 10
-cUnt_mA = pi.get_model(pi.X,pi.Y, "circulantunitary", "A"); % 11
-cSym_mA = pi.get_model(pi.X,pi.Y, "circulantsymmetric", "A"); % 12
-cSSm_mA = pi.get_model(pi.X,pi.Y, "circulantskewsymmetric", "A"); % 13 
-%BCCB_mA  = pi.get_model(pi.X, pi.Y, "BCCB", "A:s[1 1]", [1 1]); % 14
-%C_mA  = pi.get_model(pi.X, pi.Y, "TLS"); % 15
-%BC_mA  = pi.get_model(pi.X, pi.Y, "BC", "A:s[2 2]", [2 2]); % 16
-%C_mA  = pi.get_model(pi.X, pi.Y, "BCtri"); % 17
-%C_mA  = pi.get_model(pi.X, pi.Y, "BCtls"); % 18
-%C_mA  = pi.get_model(pi.X, pi.Y, ""); % 19
-%C_mA  = pi.get_model(pi.X, pi.Y, "BCCBtls"); 
-%C_mA  = pi.get_model(pi.X, pi.Y, "BCCBskewsymmetric"); 
-%C_mA  = pi.get_model(pi.X, pi.Y, "BCCBunitary"); 
-%C_mA  = pi.get_model(pi.X, pi.Y, "hankel"); 
-%C_mA  = pi.get_model(pi.X, pi.Y, "toeplitz"); 
-%C_mA  = pi.get_model(pi.X, pi.Y, "symmetric"); 
-%C_mA  = pi.get_model(pi.X, pi.Y, "skewsymmetric"); 
+kp.gen_dset(kp.X, kp.Y, nSets); % 
+kp.get_Phi();
 
-% HAVOK methods @todo investiage basis functions 
-%hvk_mA  = hvk.est(hvk.x, hvk.r, "HAVOK");
+
 
 %% results
-% piDMD models
 dlgr.add_mdl(gt_m);
-dlgr.add_mdl(pi_m); % piDMD baseline
-%dlgr.add_mdl(A_mA); % pot sol A
-%dlgr.add_mdl(B_mA);
-%dlgr.add_mdl(C_mA);
-%dlgr.add_mdl(extS_mA);
-%dlgr.add_mdl(orth_mA);
-%dlgr.add_mdl(orth_mB);
-%dlgr.add_mdl(diag_mA);
-%dlgr.add_mdl(diag_mB);
-%dlgr.add_mdl(diag_mC);
-%dlgr.add_mdl(diag_mD);
-%dlgr.add_mdl(diag_mE);
-dlgr.add_mdl(circ_mA);
-dlgr.add_mdl(cTLS_mA);
-dlgr.add_mdl(cUnt_mA);
-dlgr.add_mdl(cSym_mA);
-dlgr.add_mdl(cSSm_mA);
-%dlgr.add_mdl(BC_mA  );
-
-
-% HAVOK models
-%dlgr.add_mdl(hvk_mA);
+dlgr.add_mdl(kp_m); % 
 
 %% post processing 
 dlgr.get_errs(); 
 dlgr.get_tab(); % get res table
-dlgr.plt_recons_grid();
-dlgr.plt_A_roots(); % overlay in one fig
-dlgr.plt_A_roots_sep(); % separate figs 
-dlgr.plt_A_bmaps_sep(); % separate figs 
-dlgr.plt_A_surfs_sep(); % separate figs 
+%dlgr.plt_recons_grid();
+%dlgr.plt_A_roots(); % overlay in one fig
+%dlgr.plt_A_roots_sep(); % separate figs 
+%dlgr.plt_A_bmaps_sep(); % separate figs 
+%dlgr.plt_A_surfs_sep(); % separate figs 
 %dlgr.plt_A_hmaps_sep(); % separate figs 
 
 %% report
